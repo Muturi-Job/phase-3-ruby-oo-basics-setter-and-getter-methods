867d42634b89f975d399387dab2c5a9cedaa9974
Job Muturi <muirimuturi17@gmail.com>
2023-05-18 09:37:34 +0300

    Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,14 @@
 class Dog
-    def initialize(name, breed)
-      @name = name
-      @breed = breed
+    def name= (name)
+        @name = name
     end
-  
     def name
-      @name
+        @name
     end
-  
-    def name=(new_name)
-      @name = new_name
+    def breed= (breed)
+        @breed = breed
     end
-  
     def breed
-      @breed
-    end
-  
-    def breed=(new_breed)
-      @breed = new_breed
+        @breed
     end
   end
